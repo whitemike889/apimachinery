557ae4b22566909ff5af429e474664c3741ebb4e
Tamal Saha <tamal@appscode.com>
2020-06-08 22:52:59 -0700

    added-all

---
+++ hack/release/refresh-repo.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# set -eou pipefail
+
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+
+K8S_VERSION=v1.18.3
+
+GITHUB_USER=${GITHUB_USER:-1gtm}
+PR_BRANCH=gomod-refresher # -$(date +%s)
+COMMIT_MSG="Update to Kubernetes $K8S_VERSION"
+
+REPO_ROOT=/tmp/gomod-refresher
+
+refresh() {
+    echo "refreshing repository: $1"
+    rm -rf $REPO_ROOT
+    mkdir -p $REPO_ROOT
+    pushd $REPO_ROOT
+    git clone --no-tags --no-recurse-submodules --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@$1.git
+    cd $(ls -b1)
+    git checkout -b $PR_BRANCH
+    gomod-gen --desired-gomod="$SCRIPT_ROOT/$K8S_VERSION/go.mod"
+    go mod -require=stash.appscode.dev/apimachinery@TAG
+    go mod tidy
+    go mod vendor
+    [ -z "$2" ] || (
+        echo "$2"
+        $2 || true
+    )
+    git add --all
+    if git diff --exit-code -s HEAD; then
+        echo "Repository $1 is up-to-date."
+    else
+        git commit -a -s -m "$COMMIT_MSG"
+        git push -u origin $PR_BRANCH -f
+        hub pull-request \
+            --labels automerge \
+            --labels release/TAG \
+            --message "$COMMIT_MSG" \
+            --message "Signed-off-by: $(git config --get user.name) <$(git config --get user.email)>" || true
+
+        /done stash.appscode.dev/apimachinery
+        # gh pr create \
+        #     --base master \
+        #     --fill \
+        #     --label automerge \
+        #     --reviewer tamalsaha
+    fi
+    popd
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "Correct usage: $SCRIPT_NAME <path_to_repos_list>"
+    exit 1
+fi
+
+if [ -x $GITHUB_TOKEN ]; then
+    echo "Missing env variable GITHUB_TOKEN"
+    exit 1
+fi
+
+# ref: https://linuxize.com/post/how-to-read-a-file-line-by-line-in-bash/#using-file-descriptor
+while IFS=, read -r -u9 repo cmd; do
+    if [ -z "$repo" ]; then
+        continue
+    fi
+    refresh "$repo" "$cmd"
+    echo "################################################################################"
+done 9<$1
